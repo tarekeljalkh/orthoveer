63af7fc45bb4b40211ad135e7989ec709c8b3478
Tarek El Jalkh <tarekeljalkh@gmail.com>
2025-01-29 21:14:55 +0200

    toastr icon fix and smtp settings

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -7,6 +7,7 @@ use App\Models\Setting;
 use App\Services\SettingsService;
 use App\Traits\FileUploadTrait;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Cache;
 
 class SettingController extends Controller
@@ -86,15 +87,40 @@ class SettingController extends Controller
                 ['key' => $key],
                 ['value' => $value]
             );
+            $this->updateEnvFile(strtoupper($key), $value);
         }
 
         $settingsService = app(SettingsService::class);
         $settingsService->clearCachedSettings();
         Cache::forget('mail_settings');
 
-        toastr()->success('Updated Successfully!');
+        // Clear Laravel's config cache to reflect the new settings
+        Artisan::call('config:clear');
+
+        toastr()->success('Mail Settings Updated Successfully!');
 
         return redirect()->back();
+    }
+
+    // Function to update .env file
+    private function updateEnvFile($key, $value)
+    {
+        $envPath = base_path('.env');
+
+        if (file_exists($envPath)) {
+            $env = file_get_contents($envPath);
+
+            // Check if key exists
+            if (preg_match("/^{$key}=.*/m", $env)) {
+                // Replace the existing key-value pair
+                $env = preg_replace("/^{$key}=.*/m", "{$key}={$value}", $env);
+            } else {
+                // Append new key-value pair
+                $env .= "\n{$key}={$value}";
+            }
+
+            file_put_contents($envPath, $env);
+        }
     }
 
     function UpdateLogoSetting(Request $request)
